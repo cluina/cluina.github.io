d9e966f5b5615d3cfec51fe13fc30f8dfbd9a8b6
Sangyoung <>
2018-10-11 01:28:54 +0900

    네브바 활성화시 백드롭 추가

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,9 +1,22 @@
 let isExtended = false
 const nav = document.getElementById("toggle-nav")
 const toggleNav = () => {
-    !isExtended ? nav.classList.add('extended') : nav.classList.remove('extended')
-    isExtended = !isExtended
+    if (!isExtended) {
+        nav.classList.add('extended')
+        document.body.classList.add('nav-open')
+    } else {
+        nav.classList.remove('extended')
+        document.body.classList.remove('nav-open')
+    }
+    // setTimeout(() => {
+    //     isExtended = !isExtended
+    // }, 200)
 }
 for (item of document.getElementsByTagName('a')) {
     item.addEventListener('click' , e => toggleNav())
 }
+// document.body.addEventListener('click', (e) => {
+//     if (isExtended && !e.target.classList.contains('nav-swipe-wrapper')) {
+//         toggleNav()
+//     }
+// })
